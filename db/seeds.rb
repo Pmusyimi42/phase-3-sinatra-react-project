5a9293252bfa52343747415d46c0538a74fd66d8
patrick kisyula <pmusyimi42@gmail.com>
2023-03-06 09:53:59 +0300

    Done with seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+a1 = Author.create(name: 'Albert Einstein')
+
+q1 =Quote.create(title: "Life Style", 
+    category: "motivational", 
+    description: 'We cannot solve problems with the kind of thinking we employed when we came up with them.',
+    author_id: a1.id
+)
+u1 = User.create(username: 'Robert')
+
+c1 = Comment.create(comment: 'nice quote',
+    quote_id: q1.id,
+    user_id: u1.id
+)
 
 puts "✅ Done seeding!"
